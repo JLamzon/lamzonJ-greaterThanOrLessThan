c3de30ab3e956448630cc20b6717deb77be4c2e4
JLamzon <jlamzon@yahoo.com>
2022-10-21 10:51:43 -0700

    added peer review to program

--- Program.cs
+++ Program.cs
@@ -2,6 +2,11 @@
 //10/18/22
 //GitHubChallenge - Redo mini Challenges
 //Greater than or less than, data validation and play again
+// Peer Review: Marcel R. The program runs well, no errors or anything. 
+// It has play again functionality and will check if the number entered is an int. 
+// Also has if else statements for different scenarios and uses break to break out. 
+// One comment I have is that typing No for do you want to play again doesn't work, 
+// but adding .ToLower() would be an easy fix. Awesome work!
 
 Console.Clear();
 
@@ -71,6 +76,6 @@ while (playAgain != "no")
 
     Console.WriteLine("Press any key to play again or type no......");
 
-    playAgain = Console.ReadLine();
+    playAgain = Console.ReadLine().ToLower();
 }
 Console.WriteLine("Thanks for playing!");
